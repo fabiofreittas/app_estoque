156e23943241e6dbf95a9192ce64ef9491c365b7
Fábio Freitas <fabio.freitas@unir.br>
2018-03-01 22:04:33 -0400

    Aula 01/03/2018

--- App/DAO/ProdutoDAO.php
+++ App/DAO/ProdutoDAO.php
@@ -15,11 +15,44 @@ class ProdutoDAO extends Conexao
     {
         $sql = "insert into produtos(descricao, quantidade, valor, validade) VALUES (:descricao, :quantidade, :valor, :validade)";
         try {
-            $i =$this->conexao->prepare($sql);
+            $i = $this->conexao->prepare($sql);
             $i->bindValue(":descricao", $produto->getDescricao());
             $i->bindValue(":quantidade", $produto->getQuantidade());
             $i->bindValue(":valor", $produto->getValor());
             $i->bindValue(":validade", $produto->getValidade());
+            $i->execute();
+            return true;
+
+        } catch (\PDOException $e) {
+            echo "<div class='alert alert-danger'> {$e->getMessage()}</div>";
+
+        }
+
+    }
+
+    public function pesquisar($produto=null)
+    {
+        $sql = "select * from produtos WHERE descricao LIKE :descricao";
+        try {
+            $p=$this->conexao->prepare($sql);
+            $p->bindValue(":descricao", "%" .$produto->getDescricao()."%");
+            $p->execute();
+            return $p->fetchAll(\PDO::FETCH_CLASS, "\APP\Model\Produto");
+
+        } catch (\PDOException $e) {
+
+            echo "<div class='alert-danger'>{$e->getMessage()}</div> ";
+
+        }
+
+    }
+
+    public function excluir($produto)
+    {
+        $sql = "delete from  produtos where  id= :id";
+        try {
+            $i = $this->conexao->prepare($sql);
+            $i->bindValue("id", $produto->getId());
             $i->execute();
             return true;
 
--- view/produto-excluir.php
+++ view/produto-excluir.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marco
- * Date: 23/02/2018
- * Time: 22:45
- */
+include "../vendor/autoload.php";
+
+$p = new \App\Model\Produto();
+$p->setId($_GET['id']);
+
+$pDAO = new \App\DAO\ProdutoDAO();
+if ($pDAO->excluir($p));
+    header("location: produto-pesquisar.php? msg=1");
